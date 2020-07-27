da75e64553d00f67b8883f6e9fb6b5c343895b26
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-28 00:03:00 +0530

    App Updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      {/* <Greet name = "Dhruv" heroName="Batman">
+        <p>This is children props.</p>
+      </Greet>
+      <Greet name = "Sakshu" heroName="Wonder Woman">
+        <button>Hey</button>
+      </Greet>
+      <Greet name = "Hrithik" heroName="Superman"/>
+      <Welcome name = "Dhruv" heroName="Batman"/>
+      <Welcome name = "Sakshu" heroName="Wonder Woman"/> */}
+      <Message />
     </div>
   );
 }
