cdce2f5785be16cf443f96f24cbdb0cd87c95d4c
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-28 00:50:39 +0530

    App Updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Greet from './components/greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 function App() {
   return (
     <div className="App">
+      {/* <FunctionClick />
+      <ClassClick /> */}
+      <EventBind />
       {/* <Greet name = "Dhruv" heroName="Batman">
         <p>This is children props.</p>
       </Greet>
@@ -18,7 +24,7 @@ function App() {
       <Welcome name = "Dhruv" heroName="Batman"/>
       <Welcome name = "Sakshu" heroName="Wonder Woman"/> */}
       {/* <Message /> */}
-      <Counter />
+      {/* <Counter /> */}
     </div>
   );
 }
