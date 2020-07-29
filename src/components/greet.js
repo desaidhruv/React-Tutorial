ea197977244190ff9223f05d51f4784aac9b1357
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-30 00:02:36 +0530

    Comments Updated

--- src/components/greet.js
+++ src/components/greet.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Greet = (props) => {
+    const {name,heroName} = props // destructuring props in functional components.
 return (
     <div>
-    <h1>Hello {props.name} a.k.a {props.heroName}.</h1>
-    {props.children}
+    <h1>Hello {name} a.k.a {heroName}.</h1>
     </div>
 ) 
 }
