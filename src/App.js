f40716603a949efe3ae0c6c7cb4f8f74f8e270d6
Dhruv Desai <dhruvshah28@gmail.com>
2020-07-30 00:03:28 +0530

    Child Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/Welcome';
@@ -8,12 +7,14 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 function App() {
   return (
     <div className="App">
       {/* <FunctionClick />
       <ClassClick /> */}
-      <EventBind />
+      {/* <EventBind /> */}
+      <ParentComponent />
       {/* <Greet name = "Dhruv" heroName="Batman">
         <p>This is children props.</p>
       </Greet>
---
+++ src/components/ChildComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// When we want to pass the data from child component to the parent component we use the arrow function which allows us to send as many parameters as we want.
+function ChildComponent(props) {
+    return (
+        <div>
+            <button onClick={() => props.greetHandler('child') }>Greet Parent</button>
+        </div>
+    )
+}
+
+export default ChildComponent
