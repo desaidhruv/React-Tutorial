3f633be5e544c3a70c05f13d8affe5c3469f4d0c
Dhruv Desai <dhruvshah28@gmail.com>
2020-08-01 21:50:48 +0530

    Inline Updated

---
+++ src/components/Inline.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const heading = {
+    fontSize: '20px',
+    color: 'blue'
+}
+function Inline() {
+    return (
+        <div>
+            <h1 style={heading}>Inline</h1>
+        </div>
+    )
+}
+
+export default Inline
