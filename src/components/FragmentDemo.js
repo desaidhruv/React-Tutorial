6f632e4b4579ef3479a0d42a0b00ebe019419eb7
Dhruv Desai <dhruvshah28@gmail.com>
2020-08-21 19:43:50 +0530

    Fragment Updated

---
+++ src/components/FragmentDemo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class FragmentDemo extends Component {
+    render() {
+        return (
+            <React.Fragment>
+            <h1>
+                Fragment Demo
+            </h1>
+            <p>Describes the Fragment component</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default FragmentDemo
