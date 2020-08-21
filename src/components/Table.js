f32d4afc3256f5d27adcb8038283b56b4a67fdf8
Dhruv Desai <dhruvshah28@gmail.com>
2020-08-21 19:41:49 +0530

    Table Updated

---
+++ src/components/Table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Columns from './Columns'
+function Table() {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <Columns />
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
