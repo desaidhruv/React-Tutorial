447f8ccbedeca17cc8684be64d0a7a6b5f0fc126
Dhruv Desai <dhruvshah28@gmail.com>
2020-08-21 18:47:09 +0530

    LifeCycleB Updated

---
+++ src/components/LifeCycleB.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class LifeCycleB extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            name: 'Dhruv'
+        }
+        console.log('Lifecyle B Const')
+    }
+    static getDerivedStateFromProps(props, state) {
+        console.log('Lifecyle B getDerivedStateFromProps')
+        return null
+    }
+    componentDidMount() {
+        console.log('Lifecyle B componentDidMount')
+    }
+    render() {
+        console.log('LifecyleB Render')
+        return (
+            <div>
+                LifeCycle
+                
+            </div>
+        )
+    }
+}
+
+export default LifeCycleB
