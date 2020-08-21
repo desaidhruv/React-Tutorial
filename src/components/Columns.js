e4cf904e6896bc23963b864a1b3b7f3b0b824190
Dhruv Desai <dhruvshah28@gmail.com>
2020-08-21 19:42:57 +0530

    Columns Updated

---
+++ src/components/Columns.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Columns() {
+    const items = []
+    return (
+        <>
+            <td>Name</td>
+            <td>Dhruv</td>
+        </>
+    )
+}
+
+export default Columns
