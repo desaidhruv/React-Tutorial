81262fc124a57263fb21033a9e159f57e58d3ec2
Dhruv Desai <dhruvshah28@gmail.com>
2020-09-05 15:02:31 +0530

    Memo Comments Updated

--- src/components/MemoComp.js
+++ src/components/MemoComp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+// What PureComponents is to class based components, Memo is to function based components.
 function MemoComp({name}) {
     console.log('Rendering Memo component')
     return (
